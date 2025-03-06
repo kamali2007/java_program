98f12308f0fd4d585729126f19fca2e4b86f6127
kamali2007 <kamalimuruganandham12@gmail.com>
2025-03-06 21:15:12 +0000

    upload

---
+++ breakstatement.java
@@ -0,0 +1,13 @@
+import java.util.Scanner; 
+class breakstatement
+ {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            if (i == 13) {
+                System.out.println("Stopping at 13");
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
